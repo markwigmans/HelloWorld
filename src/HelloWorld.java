a323b041f0fe28e609dea673ac5843af57ed58d2
Ext-Mark <mark.wigmans@capgemini.com>
2019-01-13 20:37:27 +0100

    first version

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Main class Hello world application
+ *
+ * @author MWIGMANS
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
